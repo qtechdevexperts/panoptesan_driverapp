f3a60e2eaf9f6a93d3125ec9d340ce66c268841b
Wali Khan <muhammadw.contractor@elementsgs.com>
2023-06-25 03:06:31 +0500

    google map key added

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyCSmWWeMDDtvYpoUSc_Jb8CMj3Cx-hhOGI")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
